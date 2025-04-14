139a6e493f82fdf72f695e1d9611085cf9cef49c
MorgueVC <1189600@student.roc-nijmegen.nl>
2025-04-14 20:17:58 +0200

    Crypto dashboard eerste oplevering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import './coinList.css';
+import FavToggle from "./FavToggle";
+import { Link } from "react-router-dom";
+import MarketCapChart from "./MarketCapChart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [coins, setCoins] = useState([]);
+    const [toggledStates, setToggledStates] = useState({});
+
+    useEffect(() => {
+        fetch('https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=100')
+            .then(httpResponse => httpResponse.json())
+            .then(jsonResponse => {
+                setCoins(jsonResponse.Data.LIST);
+                console.log(jsonResponse.Data.LIST);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+
+    const handleToggle = (coinId) => {
+        setToggledStates(prevState => {
+            const newState = {
+                ...prevState,
+                [coinId]: !prevState[coinId]
+            };
+            localStorage.setItem("favorites", JSON.stringify(newState)); // Svae to local storage
+            return newState;
+        });
+    };
+
+    if (!coins || !coins.length) {
+        return <div>Loading...</div>;
+    }
+
+
+    return (
+        <div>
+            <h1 style={{color: '#ffffff', margin: '40px'}}>Cryptocurrency List</h1>
+            <Link to="/favorites">
+                <button className="addFavorites">Check favorites</button>
+            </Link>
+            <ul>
+                {coins.map(coin => (
+                    <li key={coin.ID} className="crypto-styling">
+                        <span>{coin.NAME} (${parseFloat(coin.PRICE_USD).toFixed(2)})</span>
+                        <FavToggle
+                            toggled={toggledStates[coin.ID]}
+                            onToggle={() => handleToggle(coin.ID)}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CoinDetail from './CoinDetail';
+import Favorites from './Favorites';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/coin/:id" element={<CoinDetail />} />
+                <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
